591d5f6989668b6df525019ab907173544e05b76
Анастасия Жарикова <Sakiod@yandex.ru>
2022-09-25 19:20:53 +0300

    Почти финал

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 # Главная страница
 def index(request):
-    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     title = 'Это главная страница проекта Yatube'
     context = {
         'title': title,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 # Страница с постами
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    title = f'Записи сообщества {group.title}'
     context = {
         'title': title,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
