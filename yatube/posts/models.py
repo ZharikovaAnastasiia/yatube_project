1c0b9dc22ef387ccbf6ad409c98e65f706ee553b
Анастасия Жарикова <Sakiod@yandex.ru>
2022-09-25 20:15:22 +0300

    Финал. Правки 1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-# Create your models here.
+
 User = get_user_model()
 
 class Group (models.Model):
-    title = models.TextField()
+    title = models.CharField()
     slug = models.SlugField()
     description = models.TextField()
 
